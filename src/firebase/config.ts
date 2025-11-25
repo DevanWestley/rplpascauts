1c1b326b2a562477d9e08710eb61d2578b4f8b74
Devan Westley <devanwestley@mail.ugm.ac.id>
2025-11-25 14:13:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-946136099-1d838",
+  "appId": "1:696242707232:web:c297554150753848699545",
+  "apiKey": "AIzaSyAYSkM7Jtsuoq_VeTi4jcheGusfsejxfHs",
+  "authDomain": "studio-946136099-1d838.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "696242707232"
+};
